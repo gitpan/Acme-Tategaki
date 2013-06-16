8b6d80f232d1af7e3a39bfb7ed3c0ec3d1a5e860
Kazuhiro Homma <kazuph@cpan.org>
2013-06-16 06:36:17 +0000

    Import of KAZUPH/Acme-Tategaki-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-Tategaki
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KAZUPH/Acme-Tategaki-0.01.tar.gz
    gitpan-cpan-author:       KAZUPH
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,41 @@
+# NAME
+
+Acme::Tategaki - It makes a text vertically.
+
+# SYNOPSIS
+
+    $ tategaki お前は、　すでに、　死んでいる。
+    死　す　お
+    ん　で　前
+    で　に　は
+    い　、　、
+    る
+    。
+
+# DESCRIPTION
+
+Acme::Tategaki makes a text vertically.
+
+# LICENSE
+
+Copyright (C) Kazuhiro Homma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazuhiro Homma <kazuph@cpan.org>
+
+# DEPENDENCIES
+
+[Array::Transpose](http://search.cpan.org/perldoc?Array::Transpose)
+
+# SEE ALSO
+
+[flippy](https://rubygems.org/gems/flippy)
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
---
+++ lib/Acme/Tategaki.pm
@@ -0,0 +1,88 @@
+package Acme::Tategaki;
+use 5.008005;
+use strict;
+use warnings;
+use utf8;
+use Array::Transpose;
+use List::Util qw(max);
+use Encode qw/decode_utf8 encode_utf8/;
+
+our $VERSION = "0.01";
+
+my @punc             = qw(、 。 ， ．);
+my @horizontal_words = qw(ー 「 」 → ↑ ← ↓ ＝ …);
+my @vertical_words   = qw(｜ ¬ ∟ ↓ → ↑ ← ॥ ：);
+my %replace_words = map {$horizontal_words[$_] => $vertical_words[$_]} (0..$#horizontal_words);
+
+sub tategaki {
+    my $self = shift;
+    my @text = @_;
+    my $text = join '　', map{decode_utf8 $_} @text;
+
+    while (my($key, $value) = each %replace_words) {
+        $text =~ s/$key/$value/g;
+    }
+
+    @text = split /\s/, $text;
+    my $max_lengh =  max map {length $_} @text;
+    @text = map{$_ . ('　' x ($max_lengh - length $_))} @text;
+    @text = map {[split //, $_]} @text;
+    @text = transpose([@text]);
+    @text = map {join '　', reverse @$_} @text;
+    print encode_utf8 $_, "\n" for @text;
+}
+
+if ( __FILE__ eq $0 ) {
+    tategaki();
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Tategaki - It makes a text vertically.
+
+=head1 SYNOPSIS
+
+    $ tategaki お前は、　すでに、　死んでいる。
+    死　す　お
+    ん　で　前
+    で　に　は
+    い　、　、
+    る
+    。
+
+=head1 DESCRIPTION
+
+Acme::Tategaki makes a text vertically.
+
+=head1 LICENSE
+
+Copyright (C) Kazuhiro Homma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazuhiro Homma E<lt>kazuph@cpan.orgE<gt>
+
+=head1 DEPENDENCIES
+
+L<Array::Transpose>
+
+=head1 SEE ALSO
+
+L<flippy|https://rubygems.org/gems/flippy>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
